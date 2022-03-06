aafa49a0192807b1e96e9e8da7bfa855391797e6
Lforwhat <1277149488@qq.com>
2022-03-06 11:49:02 +0800

    :sparkles: finish the ref function

--- src/reactivity/__tests__/ref.spec.ts
+++ src/reactivity/__tests__/ref.spec.ts
@@ -1,5 +1,7 @@
-import{ ref } from '../src/ref'
+import{ ref, isRef, unRef, proxyRef } from '../src/ref'
 import { effect } from '../src/effect'
+import { reactive } from '../src/reactivity'
+
 describe('ref', () => {
     it("ref function", () => {
         let obj = ref(1);
@@ -22,5 +24,41 @@ describe('ref', () => {
         refObject.value = 2;
         expect(count).toBe(2);
         expect(dummy).toBe(2);
+    })
+    
+    it('isRef', () => {
+        let refObj = ref(1);
+        let basicValue  = 1;
+        let reactiveObj = reactive({
+            age: 1
+        })
+        expect(isRef(refObj)).toBe(true);
+        expect(isRef(basicValue)).toBe(false);
+        expect(isRef(reactiveObj)).toBe(false);
+    })
+
+    // unRef 的功能就是 如果参数 是 ref实例对象，那么就返回ref.value, 如果参数不是ref实例对象，那么就直接返回这个参数
+    it('unRef', () => {
+        let refObj = ref(1);
+        let basicValue = 2;
+        expect(unRef(refObj)).toBe(1);
+        expect(unRef(basicValue)).toBe(2);
+    })
+
+    it('proxyRef', () => {
+        let user = {
+            age: ref(10),
+            name: 'cai'
+        }
+        let refObj = proxyRef(user);
+        expect(user.age.value).toBe(10);
+        expect(refObj.age).toBe(10);
+        expect(refObj.name).toBe('cai');
+        refObj.age = 20;
+        expect(user.age.value).toBe(20);
+        expect(refObj.age).toBe(20);
+        refObj.age = ref(30);
+        expect(refObj.age).toBe(30);
+        expect(user.age.value).toBe(30);
     })
 })
--- src/reactivity/src/ref.ts
+++ src/reactivity/src/ref.ts
@@ -7,6 +7,7 @@ class RefImpl {
     private _value : any;
     public deps;
     private _rawValue: any;
+    private _V_isRef = true; // 用来判断一个对象是否是ref
     constructor(value) {
         this._rawValue = value;
         this._value = convert(value);
@@ -41,4 +42,28 @@ function triggerRefValue(ref) {
 
 function convert(value) {
     return isObject(value) ? reactive(value) : value
+}
+
+export function isRef(ref) {
+    return !!ref._V_isRef;
+}
+
+// 如果参数是ref实例对象，返回ref.value的值， 如果参数不是ref实例对象，那么直接返回参数的值
+export function unRef(ref) {
+    return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRef(objectWithRef) {
+    return new Proxy(objectWithRef, {
+        get(target, key) {
+            return unRef(target[key]);
+        },
+        set(target, key, value, receiver) {
+            if(isRef(target[key]) && !isRef(value)) {
+                return target[key].value = value;
+            } else {
+                return Reflect.set(target, key, value, receiver);
+            }
+        }
+    })
 }
